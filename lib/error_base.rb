0e6c8bccf5ec0de8174e0d014ac7b75599d62b55
IzumiSy <beetle-noise@gmx.com>
2016-05-15 21:21:48 +0900

    Fix typo

--- lib/error_base.rb
+++ lib/error_base.rb
@@ -1,6 +1,6 @@
 class ErrorBase < StandardError
   def initialize( *message )
-    _v = v.join(' ')
+    _v = message.join(' ')
     @_msg = !_v.empty? ? _v : self.class::MESSAGE
     @_code = self.class::CODE
   end
