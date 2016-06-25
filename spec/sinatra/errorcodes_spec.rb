e94652be84bda068d5ec2eece2e62b90f48f8653
IzumiSy <beetle-noise@gmx.com>
2016-06-25 14:42:44 +0900

    Remove unused spec

--- spec/sinatra/errorcodes_spec.rb
+++ spec/sinatra/errorcodes_spec.rb
@@ -4,8 +4,4 @@ describe Sinatra::Errorcodes do
   it 'has a version number' do
     expect(Sinatra::Errorcodes::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
