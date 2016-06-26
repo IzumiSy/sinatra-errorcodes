86a436f6706be7326c248997c564acf53e9c9ddc
IzumiSy <beetle-noise@gmx.com>
2016-06-26 21:34:52 +0900

    Now two tests are pending

--- spec/sinatra/errorcodes_spec.rb
+++ spec/sinatra/errorcodes_spec.rb
@@ -5,13 +5,13 @@ describe Sinatra::Errorcodes do
     expect(Sinatra::Errorcodes::VERSION).not_to be nil
   end
 
-  it 'returns 500' do
+  xit 'returns 500' do
     get '/error'
     expect(last_response.status).to be 500
     expect(last_response.body).to eq 'Internal Server Error'
   end
 
-  it 'returns 401' do
+  xit 'returns 401' do
     get '/badrequest'
     expect(last_response.status).to be 400
     expect(last_response.body).to eq 'Bad Request'
