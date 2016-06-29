a773937a74e95d81997df20307534d0ba5dbe017
IzumiSy <beetle-noise@gmx.com>
2016-06-30 08:08:01 +0900

    Fix and add more test

--- spec/sinatra/errorcodes_spec.rb
+++ spec/sinatra/errorcodes_spec.rb
@@ -5,13 +5,23 @@ describe Sinatra::Errorcodes do
     expect(Sinatra::Errorcodes::VERSION).not_to be nil
   end
 
-  it 'returns 500' do
+  it 'provides status 500' do
+    expect(HTTPError::InternalServerError::CODE).to be 500
+    expect(HTTPError::InternalServerError::MESSAGE).to eq 'Internal Server Error'
+  end
+
+  it 'provides status 400' do
+    expect(HTTPError::BadRequest::CODE).to be 400
+    expect(HTTPError::BadRequest::MESSAGE).to eq 'Bad Request'
+  end
+
+  it 'returns status 500 ' do
     get '/error'
     expect(last_response.status).to be 500
     expect(last_response.body).to eq 'Internal Server Error'
   end
 
-  it 'returns 401' do
+  it 'returns status 400' do
     get '/badrequest'
     expect(last_response.status).to be 400
     expect(last_response.body).to eq 'Bad Request'
